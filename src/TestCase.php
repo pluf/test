ac2cd802f948fe3aa48c5377da1c2083933a7a37
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-03-10 17:40:05 +0330

    Pluf unit test class is enabled to use in tests

--- src/TestCase.php
+++ src/TestCase.php
@@ -2,6 +2,7 @@
 declare(strict_types = 1);
 namespace Pluf\Test;
 
+use Pluf_HTTP_Response;
 use Pluf_FileUtil;
 define("IN_UNIT_TESTS", true);
 
@@ -22,5 +23,98 @@ class TestCase extends \PHPUnit\Framework\TestCase
         if (! mkdir($tmp_path, 0777, true)) {
             die('Failed to create temp folder...');
         }
+    }
+
+    /**
+     * Check if this is a Pluf_Model
+     *
+     * @param Pluf_HTTP_Response $response
+     * @param string $message
+     */
+    public static function assertResponseAsModel($response, $message = ''): void
+    {
+        static::assertJson($response->content, $message);
+        $actual = json_decode($response->content, true);
+        static::assertArrayHasKey('id', $actual, $message);
+        static::assertTrue($actual['id'] > 0, $message);
+    }
+
+    /**
+     *
+     * @param Pluf_HTTP_Response $response
+     * @param int $code
+     * @param string $message
+     */
+    public static function assertResponseStatusCode(Pluf_HTTP_Response $response, $code, $message = 'Status code is not fit'): void
+    {
+        static::assertEquals($response->status_code, $code, $message);
+    }
+
+    /**
+     *
+     * @param Pluf_HTTP_Response $response
+     * @param string $message
+     */
+    public static function assertResponseNotNull(Pluf_HTTP_Response $response, $message = 'Response is null'): void
+    {
+        static::assertNotNull($response, $message);
+        static::assertNotNull($response->content, $message);
+    }
+
+    /**
+     *
+     * @param Pluf_HTTP_Response $response
+     * @param string $message
+     */
+    public static function assertResponsePaginateList(Pluf_HTTP_Response $response, $message = ''): void
+    {
+        static::assertJson($response->content, $message);
+        $actual = json_decode($response->content, true);
+        static::assertArrayHasKey('items', $actual, $message);
+        static::assertArrayHasKey('current_page', $actual, $message);
+        static::assertArrayHasKey('page_number', $actual, $message);
+    }
+
+    /**
+     *
+     * @param Pluf_HTTP_Response $response
+     * @param string $message
+     */
+    public static function assertResponseEmptyPaginateList(Pluf_HTTP_Response $response, $message = ''): void
+    {
+        static::assertJson($response->content, $message);
+        $actual = json_decode($response->content, true);
+        static::assertArrayHasKey('items', $actual, $message);
+        static::assertArrayHasKey('current_page', $actual, $message);
+        static::assertArrayHasKey('page_number', $actual, $message);
+        static::assertTrue(count($actual['items']) == 0, $message);
+    }
+
+    /**
+     *
+     * @param Pluf_HTTP_Response $response
+     * @param string $message
+     */
+    public static function assertResponseNonEmptyPaginateList(Pluf_HTTP_Response $response, $message = ''): void
+    {
+        static::assertJson($response->content, $message);
+        $actual = json_decode($response->content, true);
+        static::assertArrayHasKey('items', $actual, $message);
+        static::assertArrayHasKey('current_page', $actual, $message);
+        static::assertArrayHasKey('page_number', $actual, $message);
+        static::assertTrue(count($actual['items']) > 0, $message);
+    }
+
+    /**
+     *
+     * @param Pluf_HTTP_Response $response
+     * @param string $message
+     */
+    public static function assertResponseNotAnonymousModel(Pluf_HTTP_Response $response, $message = ''): void
+    {
+        static::assertJson($response->content, $message);
+        $actual = json_decode($response->content, true);
+        static::assertArrayHasKey('id', $actual, $message);
+        static::assertTrue($actual['id'] > 0, $message);
     }
 }
